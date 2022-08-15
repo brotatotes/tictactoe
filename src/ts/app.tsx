b5c6cd5a9255dbbf1724a5a723943d383a8fb121
brotatotes <erichao2018@gmail.com>
2022-08-15 01:37:26 -0700

    add tic tac toe class, trying to get buttons to work

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { styled } from '@mui/material/styles';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import CloseIcon from '@mui/icons-material/Close';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import { AppBar, Box, Button, Container, Grid, Paper, Toolbar, Typography } from '@mui/material';
+import { TTTGame, TTTPiece } from './tictactoe';
 
 const darkTheme = createTheme({
   palette: {
@@ -13,36 +13,31 @@ const darkTheme = createTheme({
   },
 });
 
-enum TicTacToePiece {
-  X,
-  O,
-  None
-}
-
 class App extends React.Component<{}> {
-  state = {};
+  state = {
+    tictactoe : new TTTGame()
+  };
 
-  testArray: TicTacToePiece[] = [
-    TicTacToePiece.X, TicTacToePiece.None, TicTacToePiece.X,
-    TicTacToePiece.O, TicTacToePiece.O, TicTacToePiece.None,
-    TicTacToePiece.None, TicTacToePiece.None, TicTacToePiece.None,
-  ]
-
-  getSquare(piece: TicTacToePiece)
+  getSquare(piece: TTTPiece, index: number)
   {
     let icon = <div/>
-    if (piece === TicTacToePiece.X) {
+    if (piece === TTTPiece.X) {
       icon = <CloseIcon sx={{ fontSize: "85px" }}/>
-    } else if (piece === TicTacToePiece.O) {
+    } else if (piece === TTTPiece.O) {
       icon = <RadioButtonUncheckedIcon sx={{ fontSize: "70px" }}/>
     }
 
-    return <Button fullWidth sx={{
-      minHeight: 140,
-      textAlign: 'center',
-    }}>
+    return <Button fullWidth sx={{ minHeight: 140, textAlign: 'center', }} onClick={(e) => this.clickSquare(index)}>
       {icon}
     </Button>
+  }
+
+  clickSquare(index: number) {
+    if (this.state.tictactoe.getPiece(index)) {
+      this.state.tictactoe.setPiece(index, TTTPiece.X)
+      console.log(this.state.tictactoe.getBoard())
+      this.setState({ tictactoe: this.state.tictactoe })
+    }
   }
 
   render() {
@@ -59,8 +54,8 @@ class App extends React.Component<{}> {
           }}
         >
           <Toolbar>
-            <Typography variant="h6" color="inherit" noWrap>
-              <h1>tic tac toe</h1>
+            <Typography variant="h2" color="inherit" noWrap>
+              tic tac toe
             </Typography>
           </Toolbar>
         </AppBar>
@@ -68,13 +63,13 @@ class App extends React.Component<{}> {
           <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
             <Box sx={{ flexGrow: 1 }}>
               <Grid container spacing={4}>
-                {this.testArray.map(piece =>
-                  <Grid item xs={4}>
+                {this.state.tictactoe.getBoard().map((piece, index) =>
+                  <Grid key={index} item xs={4}>
                     <Paper sx={{
                       backgroundColor: '#1A2027',
                       textAlign: 'center',
                     }}>
-                      {this.getSquare(piece)}
+                      {this.getSquare(piece, index)}
                     </Paper>
                   </Grid>
                 )}
