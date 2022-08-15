39d474a4e86f64a96902c823fb81550b773c0e90
brotatotes <erichao2018@gmail.com>
2022-08-15 13:12:59 -0700

    moved tictactoe to its own tsx

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import CloseIcon from '@mui/icons-material/Close';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
-import { AppBar, Box, Button, Container, Grid, Paper, Toolbar, Typography } from '@mui/material';
-import { TTTGame, TTTPiece } from './tictactoe';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { TicTacToe } from './tictactoe';
 
 const darkTheme = createTheme({
   palette: {
@@ -14,31 +12,7 @@ const darkTheme = createTheme({
 });
 
 class App extends React.Component<{}> {
-  state = {
-    tictactoe : new TTTGame()
-  };
-
-  getSquare(piece: TTTPiece, index: number)
-  {
-    let icon = <div/>
-    if (piece === TTTPiece.X) {
-      icon = <CloseIcon sx={{ fontSize: "85px" }}/>
-    } else if (piece === TTTPiece.O) {
-      icon = <RadioButtonUncheckedIcon sx={{ fontSize: "70px" }}/>
-    }
-
-    return <Button fullWidth sx={{ minHeight: 140, textAlign: 'center', }} onClick={(e) => this.clickSquare(index)}>
-      {icon}
-    </Button>
-  }
-
-  clickSquare(index: number) {
-    if (this.state.tictactoe.getPiece(index)) {
-      this.state.tictactoe.setPiece(index, TTTPiece.X)
-      console.log(this.state.tictactoe.getBoard())
-      this.setState({ tictactoe: this.state.tictactoe })
-    }
-  }
+  state = {}
 
   render() {
     return (
@@ -59,24 +33,7 @@ class App extends React.Component<{}> {
             </Typography>
           </Toolbar>
         </AppBar>
-        <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-          <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-            <Box sx={{ flexGrow: 1 }}>
-              <Grid container spacing={4}>
-                {this.state.tictactoe.getBoard().map((piece, index) =>
-                  <Grid key={index} item xs={4}>
-                    <Paper sx={{
-                      backgroundColor: '#1A2027',
-                      textAlign: 'center',
-                    }}>
-                      {this.getSquare(piece, index)}
-                    </Paper>
-                  </Grid>
-                )}
-              </Grid>
-            </Box>
-          </Paper>
-        </Container>
+        <TicTacToe />
       </ThemeProvider>
     )
   }
---
+++ src/ts/tictactoe.tsx
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import { Paper, Grid, Box, Button, Container } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+
+export class TicTacToe extends React.Component<{}> {
+  state = {
+    tictactoe: new TTTGame(),
+    playerTurn: true,
+    winner: TTTPiece.None
+  }
+
+  getSquare(piece: TTTPiece, index: number) {
+    let icon = <div />
+    if (piece === TTTPiece.X) {
+      icon = <CloseIcon sx={{ fontSize: "85px" }} />
+    } else if (piece === TTTPiece.O) {
+      icon = <RadioButtonUncheckedIcon sx={{ fontSize: "70px" }} />
+    }
+
+    return (
+      <Button fullWidth sx={{ minHeight: 140, textAlign: 'center', }} onClick={(e) => this.clickSquare(index)}>
+        {icon}
+      </Button>
+    )
+  }
+
+  clickSquare(index: number) {
+    if (this.state.playerTurn && this.state.tictactoe.getPiece(index) === TTTPiece.None) {
+      this.state.tictactoe.setPiece(index, TTTPiece.X)
+      let winner = this.state.tictactoe.getWinner()
+      this.setState({ tictactoe: this.state.tictactoe, playerTurn: !this.state.playerTurn, winner: winner })
+
+      console.log(this.state.tictactoe)
+      
+      let botIndex = Math.floor(Math.random() * 9)
+      while (this.state.tictactoe.getPiece(botIndex) === TTTPiece.None) {
+        botIndex = Math.floor(Math.random() * 9)
+      }
+
+      // console.log(botIndex)
+      // console.log(this.state.tictactoe)
+
+      // this.state.tictactoe.setPiece(botIndex, TTTPiece.O)
+      // winner = this.state.tictactoe.getWinner()
+      // this.setState({ tictactoe: this.state.tictactoe, playerTurn: !this.state.playerTurn, winner: winner })
+    }
+  }
+
+  render() {
+    return (
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={4}>
+              {this.state.tictactoe.getBoard().map((piece, index) =>
+                <Grid key={index} item xs={4}>
+                  <Paper sx={{
+                    backgroundColor: '#1A2027',
+                    textAlign: 'center',
+                  }}>
+                    {this.getSquare(piece, index)}
+                  </Paper>
+                </Grid>
+              )}
+            </Grid>
+          </Box>
+        </Paper>
+      </Container>
+    )
+  }
+}
+
+enum TTTPiece {
+  None = 0,
+  X,
+  O,
+}
+
+class TTTGame {
+  private board: TTTPiece[]
+
+  constructor() {
+    this.board = new Array(9).fill(TTTPiece.None)
+  }
+
+  public getBoard() {
+    return this.board
+  }
+
+  public getPiece(index: number) {
+    return this.board[index]
+  }
+
+  public setPiece(index: number, piece: TTTPiece) {
+    this.board[index] = piece
+  }
+
+  public getWinner(): TTTPiece {
+    return this.checkRowWinner() || this.checkColWinner() || this.checkDiagWinner()
+  }
+
+  private checkRowWinner(): TTTPiece {
+    for (let r = 0; r < 3; r++) {
+      let winner = this.checkArrWinner([r * 3, r * 3 + 1, r * 3 + 2])
+      if (winner) {
+        return winner
+      }
+    }
+
+    return TTTPiece.None
+  }
+
+  private checkColWinner(): TTTPiece {
+    for (let c = 0; c < 3; c++) {
+      let winner = this.checkArrWinner([c, c + 3, c + 6])
+      if (winner) {
+        return winner
+      }
+    }
+
+    return TTTPiece.None
+  }
+
+  private checkDiagWinner(): TTTPiece {
+    return this.checkArrWinner([0, 4, 8]) || this.checkArrWinner([2, 4, 6])
+  }
+
+  private checkArrWinner(arr: number[]): TTTPiece {
+    let pieces = arr.map(i => this.board[i])
+    if (pieces.every((v) => v === pieces[0])) {
+      return pieces[0]
+    }
+
+    return TTTPiece.None
+  }
+}
